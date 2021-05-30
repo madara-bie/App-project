02f2313002ace3370f485272de1bdae0f4326b91
Madara <madara.bieza@gmail.com>
2021-05-30 19:19:54 +0300

    Testing JS

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ function showTemperature(response) {
 function searchCity(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(showTemperature);
 }
 
 
