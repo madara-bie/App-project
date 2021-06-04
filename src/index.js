b33ec9a1328d4df9a5f866f8effa61b020ad5d13
Madara <madara.bieza@gmail.com>
2021-06-04 21:10:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -32,21 +32,20 @@ function showForecast() {
   forecastElement.innerHTML = `
     <div class="row">
       <div class="col-2">
-        <div class="weather-forecast-date">Thu</div>
+        <div class="weather-forecast-date">
+          Thu
+        </div>
           <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="">
-          div class="weather-forecast-temperatures">
+        div class="weather-forecast-temperatures">
             <span class="weather-forecast-max">
               18°
             </span>
             <span class="weather-forecast-min">
               15°
             </span>
-          </div>  
         </div>
       </div>
-    </div>
-  </div>`;
-
+    </div>`;
 }
 
 function searchCity(city) {
