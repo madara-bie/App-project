05ddf135e54637d698ae0967d9d7ef843c8f557f
Madara <madara.bieza@gmail.com>
2021-06-05 16:26:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function showCity(event) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sut"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   return days[day];
 }
