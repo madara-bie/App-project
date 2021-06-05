9a1b7b37680c2bf08204eec06e262a25f4ac70a0
Madara <madara.bieza@gmail.com>
2021-06-05 15:58:16 +0300

    Adding API to the forecast

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ function showCity(event) {
   searchCity(city);
 }
 
-function showForecast() {
+function showForecast(response) {
+  console.log(response.data);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
@@ -38,6 +39,7 @@ function getForecast(coordinates) {
   let apiId = "d547f7175aa4839fd00918dad2121b28";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiId}&units-metric`;
   console.log(apiUrl);
+  axios.get(apiId).then(showForecast);
 }
 
 function showTemperature(response) {
@@ -121,4 +123,3 @@ celsiusLink.addEventListener("click", showCelsiusTemperature);
 
 searchCity("London");
 updateCurrentTime();
-showForecast();
